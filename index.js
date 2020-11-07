7fdd55d1ffb12e00a7665cf31f2c37c9bee1d5ee
astronomical-queue-4656 <halierae17@outlook.com>
2020-11-07 19:35:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!";
   } else if (string.toUpperCase() === string) {
-    
-  }
+    return "YES INDEED!";
+  } 
 }
